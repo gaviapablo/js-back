2c768633edcc9a529882bbf5f4da94fb521b4a16
gaviapablo <gaviapablo@hotmail.com>
2023-03-24 11:17:06 -0300

    open db

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
+import { openDb } from './configDB.js';
 
+import express from 'express';
 const app = express();
-
 const PORT = 3000;
+
+openDb();
 
 app.get('/', (req,res) => {
     console.log("get recebido")
+    res.send(req.hostname)
+})
+
+app.post('/', (req,res) => {
+    console.log("post recebido")
+    res.send(req.hostname)
+})
+
+app.put('/', (req,res) => {
+    console.log("putrecebido")
+    res.send(req.hostname)
+})
+
+app.delete('/', (req,res) => {
+    console.log("delete recebido")
     res.send(req.hostname)
 })
 
